83e0ad311e913f7c99d1cce0c8543a3e163cceec
Your Name <you@example.com>
2020-08-06 17:12:16 +0300

    first

---
+++ src/components/Navbar.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import AppBar from "@material-ui/core/AppBar";
+import ToolBar from "@material-ui/core/Toolbar";
+import Button from "@material-ui/core/Button";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+import MyButton from "../util/MyButton";
+import {
+  Add as AddIcon,
+  Home as HomeIcon,
+  Notifications,
+} from "@material-ui/icons";
+class Navbar extends Component {
+  render() {
+    const { authenticated } = this.props;
+    return (
+      <AppBar>
+        <ToolBar className="nav-container">
+          {authenticated ? (
+            <>
+              <MyButton tip="Post a scream">
+                <AddIcon color="primary" />
+              </MyButton>
+              <Link to="/">
+                <MyButton tip="Home">
+                  <HomeIcon color="primary" />
+                </MyButton>
+              </Link>
+              <MyButton tip="Notifications">
+                <Notifications color="primary" />
+              </MyButton>
+            </>
+          ) : (
+            <>
+              <Button color="inherit" component={Link} to="/login">
+                Login
+              </Button>
+              <Button color="inherit" component={Link} to="/">
+                Home
+              </Button>
+              <Button color="inherit" component={Link} to="/signup">
+                Signup
+              </Button>
+            </>
+          )}
+        </ToolBar>
+      </AppBar>
+    );
+  }
+}
+Navbar.propTypes = {
+  authenticated: PropTypes.bool.isRequired,
+};
+const mapStateToProps = (state) => ({
+  authenticated: state.user.authenticated,
+});
+export default connect(mapStateToProps)(Navbar);
